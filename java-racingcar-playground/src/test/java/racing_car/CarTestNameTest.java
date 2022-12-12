a4e4ceecb3af9a7b6cdf81b76d4daef044cfb7a9
Terry <terryakishin0814@gmail.com>
2022-12-12 22:33:01 +0900

    refactor(all) : 불필요한 import 제거

--- java-racingcar-playground/src/test/java/racing_car/CarTestNameTest.java
+++ java-racingcar-playground/src/test/java/racing_car/CarTestNameTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import racing_car.CarName;
 
 public class CarTestNameTest {
 
