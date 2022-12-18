076dee357b62a467dfef3a87d50116b0ac78e4db
Terry <terryakishin0814@gmail.com>
2022-12-18 21:30:23 +0900

    refactor(CarMoving) : 테스크 재구조화

--- java-racingcar-playground/src/test/java/racing_car/domain/car/CarMovingTest.java
+++ java-racingcar-playground/src/test/java/racing_car/domain/car/CarMovingTest.java
@@ -4,26 +4,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Test;
 
 
 @DisplayName("CarMoving Class")
 public class CarMovingTest {
 
-  private CarMoving carMoving;
-
-  @BeforeEach
-  void setUp(){
-    carMoving = new CarMoving();
+  @DisplayName("is instantiated with new CarMoving()")
+  @Test
+  void isInstantiatedWithNew() {
+    new CarMoving();
   }
 
-  @DisplayName("차량 이동 테스트")
-  @ParameterizedTest
-  @CsvSource(value = {"1,0", "9,1"})
-  void 차량_이동(int given, int expected){
-    carMoving.goForward(given);
-    assertThat(carMoving.getPosition()).isEqualTo(expected);
+  @Nested
+  @DisplayName("when new")
+  class WhenNew{
+    private CarMoving carMoving;
+
+    @BeforeEach
+    void setUp(){
+      carMoving = new CarMoving();
+    }
+
+    @DisplayName("차량 이동 테스트")
+    @RepeatedTest(value = 9, name = "{displayName} - {currentRepetition}값이 5이상이면 1칸 전진")
+    void 차량_이동(RepetitionInfo repetitionInfo){
+      int expect = 0;
+      if(repetitionInfo.getCurrentRepetition() > 4) expect = 1;
+
+      carMoving.goForward(repetitionInfo.getCurrentRepetition());
+      assertThat(carMoving.getPosition()).isEqualTo(expect);
+    }
+
   }
 
 }
